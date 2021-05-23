527f62b9f306503a6fc0f41d1e64d55b8fad121f
ShivKokkula <shivbk987@gmail.com>
2021-05-23 16:27:34 +0530

    Refactor - added equals method in the program

--- LineComparison.java
+++ LineComparison.java
@@ -6,12 +6,24 @@ public class LineComparison {
 		
 		System.out.println("Welcome to Line Comparison Computation Program");
 		double line1 = lineLenght(0.0,0.0,8.0,1.0);
-		
-		System.out.println("Lenght of line is :" + line1);
+		double line2 = lineLenght(0.0,0.0,8.0,1.0);
+		equals(line1,line2);
 	}
 
 	public static double lineLenght(double x1, double y1, double x2, double y2) {
 		 double lenght = Math.sqrt(Math.pow((x2 - x1),2) - Math.pow((y2 - y1),2));
 		 return lenght;	
+	}
+	
+	public static void equals(double lineOneLenght, double lineTwoLenght) {
+		String line1 = Double.toString(lineOneLenght);
+		String line2 = Double.toString(lineTwoLenght);
+		
+		if(line1.equals(line2)) {
+			System.out.println("Two lines are equal");
+		}else {
+			System.out.println("Two lines are not equal");
+		}
+		
 	}
 }
