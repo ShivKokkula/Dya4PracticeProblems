71ef302ac5544a5d4ba7d9b6f99380ca9aed56ef
ShivKokkula <shivbk987@gmail.com>
2021-05-23 16:46:09 +0530

    Refactor - added compareTo method in the program

--- LineComparison.java
+++ LineComparison.java
@@ -5,10 +5,9 @@ public class LineComparison {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Line Comparison Computation Program");
-		double line1 = lineLenght(0.0,0.0,8.3,1.0);
-		double line2 = lineLenght(0.0,0.0,8.4,1.0);
+		double line1 = lineLenght(0.0,0.0,8.0,1.0);
+		double line2 = lineLenght(0.0,0.0,8.0,1.0);
 		equals(line1,line2);
-		compareTo(line1,line2);
 	}
 
 	public static double lineLenght(double x1, double y1, double x2, double y2) {
@@ -24,22 +23,6 @@ public class LineComparison {
 			System.out.println("Two lines are equal");
 		}else {
 			System.out.println("Two lines are not equal");
-		}
-		
-	}
-	
-	public static void compareTo(double lineOneLenght, double lineTwoLenght) {
-		String line1 = Double.toString(lineOneLenght);
-		String line2 = Double.toString(lineTwoLenght);
-		
-		int result = line1.compareTo(line2);
-		
-		if(result == 0) {
-			System.out.println("Two lines are equal");
-		}else if(result > 0) {
-			System.out.println("Line1 is greater than line2");
-		}else {
-			System.out.println("Line1 is smaller than line2");
 		}
 		
 	}
